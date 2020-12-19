98b1aad2d1eeda17901fc07824fcb9ac5ee0c489
efsam <E.salimi75@ymail.com>
2020-12-19 20:18:00 +0330

    commit ConvertorTest in secondary

--- src/test/java/ConvertorTest.java
+++ src/test/java/ConvertorTest.java
@@ -10,15 +10,30 @@ public class ConvertorTest {
     public void convertStringToIntegerTest() throws Exception {
         Integer result = Convertor.convertStringToInteger("300");
         Assert.assertEquals(300, result.intValue());
+
+        Integer result2 = Convertor.convertStringToInteger("32767");
+        Assert.assertEquals(32767, result2.intValue());
+
+        //Integer result3 = Convertor.convertStringToInteger("32768");
+        // Assert.assertEquals(32768, result3.intValue());
+
     }
 
     @Test
-    public void onlyNumberTest(){
+    public void onlyNumberTest() {
         Assert.assertTrue(Convertor.onlyNumber("123"));
         Assert.assertTrue(Convertor.onlyNumber("-12"));
         Assert.assertTrue(Convertor.onlyNumber("100"));
         Assert.assertTrue(Convertor.onlyNumber("3"));
         Assert.assertTrue(Convertor.onlyNumber("-8"));
-        //Assert.assertTrue(Convertor.onlyNumber("2s3"));
+    }
+
+    @Test(expected = AssertionError.class)
+    public void onlyNumberTest_False() {
+        Assert.assertTrue(Convertor.onlyNumber("12 3"));
+        Assert.assertTrue(Convertor.onlyNumber("-1 2"));
+        Assert.assertTrue(Convertor.onlyNumber("1.00"));
+        Assert.assertTrue(Convertor.onlyNumber("3sg"));
+        Assert.assertTrue(Convertor.onlyNumber("-8p"));
     }
 }
