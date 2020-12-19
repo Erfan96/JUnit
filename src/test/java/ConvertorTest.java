578f449219a74ee01ee244e6bbbcccb0bac62b36
efsam <E.salimi75@ymail.com>
2020-12-19 19:29:09 +0330

    commit ConvertorTest in secondary

--- src/test/java/ConvertorTest.java
+++ src/test/java/ConvertorTest.java
@@ -8,12 +8,7 @@ public class ConvertorTest {
 
     @Test
     public void convertStringToIntegerTest(){
-        Assert.assertTrue(Convertor.onlyNumber("123"));
-        Assert.assertTrue(Convertor.onlyNumber("-12"));
-        Assert.assertTrue(Convertor.onlyNumber("100"));
-        Assert.assertTrue(Convertor.onlyNumber("3"));
-        Assert.assertTrue(Convertor.onlyNumber("-8"));
-        Assert.assertTrue(Convertor.onlyNumber("2s3"));
+
     }
 
     @Test
